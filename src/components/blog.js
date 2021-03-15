019a749ca11da2540034b9a7e60da2cecd411cd8
ChrisKnowsDev <chrisharley81@gmail.com>
2021-03-15 19:50:39 -0400

    page query for blog data home page and displayed latest three blogs on home page, setup blogs, and blog component

--- src/components/blog.js
+++ src/components/blog.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { Link } from 'gatsby'
 
-const Blog = ({ content, data, description, id, image, key, slug }) => {
+const Blog = ({ category, date, description, id, image, key, slug, title }) => {
   const blogImage = getImage(image)
-
+  console.log(title)
   return (
     <Link to={`/blogs/${slug}`}>
-      <article>blog component</article>
+      <article>{image && <GatsbyImage image={blogImage} />}</article>
+      <div className="blog-card">
+        <h4>{title}</h4>
+        <p>{description}</p>
+        <div className="blog-footer">
+          <p>{category}</p>
+          <p>{date}</p>
+        </div>
+      </div>
     </Link>
   )
 }
