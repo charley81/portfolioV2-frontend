90d824d72f81cb38e4a53d4c34d47d0b72901c4a
ChrisKnowsDev <chrisharley81@gmail.com>
2021-03-25 16:02:25 -0400

    styling sidebar

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -127,9 +127,9 @@ const globalStyles = css`
     border-bottom: 0.5rem solid var(--secondaryColor);
   }
 
-  ${'' /* Sidebar */}
+  /* Sidebar */
   .sidebar {
-    background: var(--clr-grey-10);
+    background: var(--lightColor);
     position: fixed;
     top: 0;
     left: 0;
@@ -142,28 +142,56 @@ const globalStyles = css`
     transition: var(--transition);
     transform: translateX(-100%);
   }
+
   .show-sidebar {
     opacity: 1;
     transform: translateX(0);
   }
+
   .sidebar-links li {
     opacity: 0;
   }
+
   .sidebar-links li a {
     display: block;
     text-align: center;
     text-transform: capitalize;
-    color: var(--clr-grey-5);
-    letter-spacing: var(--spacing);
+    color: var(--darkColor);
     margin-bottom: 0.5rem;
     font-size: 2rem;
     transition: var(--transition);
-    border-radius: var(--radius);
+    border-radius: var(--borderRadius);
+    border: 1px solid transparent;
   }
+
   .sidebar-links li a:hover {
-    background: var(--clr-primary-9);
-    color: var(--clr-primary-5);
+    border: 1px solid var(--primaryColor);
+    color: var(--primaryColor);
   }
+
+  .sidebar-icons {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 2rem;
+
+    a {
+      color: var(--secondaryColor);
+      margin-right: 2rem;
+      font-size: 2rem;
+      padding: 0.75rem;
+      background: var(--primaryColor);
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      transition: var(--transition);
+
+      &:hover {
+        background: var(--secondaryColor);
+        color: var(--primaryColor);
+      }
+    }
+  }
+
   .close-btn {
     position: absolute;
     right: 4.75%;
@@ -171,34 +199,41 @@ const globalStyles = css`
     font-size: 2.5rem;
     background: transparent;
     border-color: transparent;
-    color: var(--clr-red-dark);
+    color: var(--primaryColor);
     cursor: pointer;
   }
+
   @media screen and (min-width: 992px) {
     .sidebar {
       transform: translateX(-100%);
     }
   }
 
-  ${'' /* Sidebar Animation */}
+  /* Sidebar Animation */
   .sidebar-links li {
     animation: slideRight 0.5s ease-in-out 0.3s forwards;
   }
+
   .sidebar-links li:nth-of-type(1) {
     animation-delay: 0.25s;
   }
+
   .sidebar-links li:nth-of-type(2) {
     animation-delay: 0.5s;
   }
+
   .sidebar-links li:nth-of-type(3) {
     animation-delay: 0.75s;
   }
+
   .sidebar-links li:nth-of-type(4) {
     animation-delay: 1s;
   }
+
   .sidebar-links li:nth-of-type(5) {
     animation-delay: 1.25s;
   }
+
   @keyframes slideRight {
     0% {
       transform: translateX(-200px);
@@ -209,25 +244,32 @@ const globalStyles = css`
       opacity: 1;
     }
   }
+
   .sidebar-icons li {
     opacity: 0;
     animation: slideUp 0.5s ease-in-out 0.3s forwards;
   }
+
   .sidebar-icons li:nth-of-type(1) {
     animation-delay: 0.25s;
   }
+
   .sidebar-icons li:nth-of-type(2) {
     animation-delay: 0.5s;
   }
+
   .sidebar-icons li:nth-of-type(3) {
     animation-delay: 0.75s;
   }
+
   .sidebar-icons li:nth-of-type(4) {
     animation-delay: 1s;
   }
+
   .sidebar-icons li:nth-of-type(5) {
     animation-delay: 1.25s;
   }
+
   @keyframes slideUp {
     0% {
       transform: translateY(200px);
