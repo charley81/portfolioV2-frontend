eed3f55e1d0fcab1cea0bbb6f29577f06f4b389e
ChrisKnowsDev <chrisharley81@gmail.com>
2021-03-25 18:25:32 -0400

    form page... UI/Styling, formspree setup and testing

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,64 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { css } from '@emotion/react'
 
 const contactPage = () => {
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <section
+        css={css`
+          display: grid;
+          place-items: center;
+          margin-top: 5rem;
+          background: linear-gradient(
+            to bottom right,
+            var(--primaryColor),
+            var(--secondaryColor)
+          );
+          border-radius: var(--borderRadius);
+          padding: 1rem;
+
+          h3 {
+            text-align: center;
+            margin-bottom: 1.25rem;
+            color: var(--lightColor);
+          }
+
+          form {
+            width: 80vw;
+            max-width: 35rem;
+          }
+
+          input,
+          textarea {
+            display: block;
+            width: 100%;
+            padding: 0.75rem 1rem;
+            margin-bottom: 1rem;
+            border-radius: var(--borderRadius);
+            border: transparent;
+          }
+        `}
+      >
+        <article>
+          <h3>Contact Me</h3>
+          <form action="https://formspree.io/f/mayawpoy" method="POST">
+            <div className="input-fields">
+              <input type="text" name="name" placeholder="Name" required />
+              <input type="email" name="email" placeholder="Email" required />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="Message"
+                required
+              ></textarea>
+            </div>
+            <button className="btn center-btn" type="submit">
+              Submit
+            </button>
+          </form>
+        </article>
+      </section>
     </Layout>
   )
 }
